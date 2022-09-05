942b0bd06e940787ed2b742bc196e9fb4943ef64
ultrasonicytb <ultrasonic2408@gmail.com>
2022-09-05 16:22:49 +0200

    push of the working main and deployer

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,11 +1,31 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
-const { clientId, token } = require('../config.json');
+const { token, clientId, guildId } = require('../config.json');
+const fs = require('node:fs');
+const path = require('node:path');
 
 const commands = [];
+const commandFiles = fs.readdirSync(path.join(__dirname, './commands/')).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(path.join(__dirname, './commands/')+file);
+	commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(data => console.log(`Successfully registered ${data.length} application commands.`))
-	.catch(console.error);
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		console.error(error);
+	}
+})();
+
